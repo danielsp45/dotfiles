c366d260a283f6649c4d231d18cdea0d60169cbb
daniel_sp <danielsp356@gmail.com>
2023-01-17 20:49:30 +0000

    Refactor vim to lua config

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,56 @@
+return require('packer').startup(function(use)
+    -- Prevent Packer from removing itself
+    use 'wbthomason/packer.nvim'
+
+    --Coc Vim
+    use {'neoclide/coc.nvim', branch = 'release'}
+
+    -- Gruvbox color scheme
+    use 'morhetz/gruvbox'
+
+    --Airline plugin 
+    use 'vim-airline/vim-airline'
+    use 'vim-airline/vim-airline-themes'
+
+    --Vim wiki
+    use 'vimwiki/vimwiki'
+
+    --Plugin to fast scrolling
+    use 'psliwka/vim-smoothie'
+    
+    --Vim commentery
+    use 'https://github.com/tpope/vim-commentary.git'
+
+    --fZf pugin
+    use 'junegunn/fzf'
+
+    --NerdTree plugin
+    use 'https://github.com/preservim/nerdtree.git'
+
+    --Plugin for elixir syntax
+    use 'elixir-editors/vim-elixir'
+
+   --Pairs plugin
+   use 'https://github.com/jiangmiao/auto-pairs.git'
+
+   --Syntax highlything
+   use 'sheerun/vim-polyglot'
+
+   --gitgutter plugin
+   use 'https://github.com/airblade/vim-gitgutter.git'
+
+   --ALE plugin 
+   use 'dense-analysis/ale'
+
+   --Toggleterm plugin
+   use 'https://github.com/akinsho/toggleterm.nvim.git'
+
+   --Fugite vim plugin
+   use 'https://github.com/tpope/vim-fugitive.git'
+
+   --Vim icons
+   use 'ryanoasis/vim-devicons'
+
+   --Treesitter
+   -- use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+end)
--- nvim/themes/airline.vim
+++
@@ -1,8 +0,0 @@
-"Current theme
-let g:airline_theme='base16_gruvbox_dark_medium'
-
-"Powerline fonts
-let g:airline_powerline_fonts=1
-let g:airline_left_sep=''
-let g:airline_right_sep=''
-
