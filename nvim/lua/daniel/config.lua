8c6cf99597a2d457789e99aa75aff9faa2b7da9a
daniel_sp <mail@danielpereira.xyz>
2024-05-28 22:54:01 +0100

    feat[nvim]: add a function to disable lsp recommendations

--- nvim/lua/daniel/config.lua
+++ nvim/lua/daniel/config.lua
@@ -33,3 +33,13 @@ vim.lsp.handlers["textDocument/signatureHelp"] =
     border = "single",
   }
 )
+
+vim.api.nvim_create_user_command("DiagnosticToggle", function()
+	local config = vim.diagnostic.config
+	local vt = config().virtual_text
+	config {
+		virtual_text = not vt,
+		underline = not vt,
+		signs = not vt,
+	}
+end, { desc = "toggle diagnostic" })
