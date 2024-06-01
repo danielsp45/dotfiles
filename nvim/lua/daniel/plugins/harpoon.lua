a189dcd8042ad8ba43829584a4c75be0d089f52b
daniel_sp <mail@danielpereira.xyz>
2024-06-01 15:29:14 +0100

    feat[nvim]: costumize nvim starter

--- nvim/lua/daniel/plugins/harpoon.lua
+++ nvim/lua/daniel/plugins/harpoon.lua
@@ -11,6 +11,7 @@ return {
 
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<leader>r", function() harpoon:list():remove() end)
+        vim.keymap.set("n", "<leader>x", function() harpoon:list():clear() end)
 
         vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
         vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
