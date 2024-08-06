9c2f2abe6addf56bead00ba6a6aeb4b72b6f0f22
daniel_sp <mail@danielpereira.xyz>
2024-08-06 13:14:57 +0100

    feat[home-manager]: add zsh config aliases

--- home-manager/common.nix
+++ home-manager/common.nix
@@ -28,7 +28,6 @@
     # Home Manager is pretty good at managing dotfiles. The primary way to manage
     # plain files is through 'home.file'.
     home.file = {
-    ".zshrc".source = ./../zsh/zshrc;
     ".config/alacritty".source = ./../alacritty;
     ".config/nvim".source = ./../nvim;
     };
--- home-manager/linux/gsd-cluster.nix
+++ home-manager/linux/gsd-cluster.nix
@@ -6,5 +6,64 @@
     home.username = "gsd";
     home.homeDirectory = "/home/gsd";
 
+    home.file = {
+    # ".zshrc".source = ./../../zsh/linux/zshrc;
+    };
+
+    programs = {
+        zsh = {
+            enable = true;
+            enableCompletion = true;
+            shellAliases = {
+                cat="bat";
+                nv="nvim";
+                lv="lvim";
+                v="nvim $(fzf)";
+                ":q"="exit";
+                cl="clear";
+
+                bs="bin/server";
+                bb="bin/build";
+                br="bin/run";
+                bt="bin/test";
+                bf="bin/format";
+                bl="bin/lint";
+                bsh="bin/console";
+                bcl="bin/clean";
+                bst="bin/setup";
+                bsr="bin/start";
+                bsp="bin/stop";
+                    
+                m="mix";
+                im="iex -S mix";
+                ms="mix phx.server";
+                mc="mix do clean, compile";
+                mf="mix format";
+                ml="mix lint";
+                mt="mix test";
+                mpr="mix phx.routes";
+                mer="mix ecto.reset";
+
+                mk="make";
+                mkh="make help";
+                mkl="make lint";
+                mkc="make clean";
+                mkr="make run";
+                mkt="make test";
+                mkut="make unit-tests";
+                mkts="make tests";
+
+            };
+        };
+
+        oh-my-zsh = {
+            enable = true;
+        };
+
+        zsh-powerlevel10k = {
+            enable = true;
+        };
+    };
+
     imports = [ ./../common.nix ];
 }
