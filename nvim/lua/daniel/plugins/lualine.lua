4ef179dfe860591e710ee4ce07650ca7115badfd
danielsp45 <mail@danielpereira.xyz>
2024-12-22 15:39:27 +0000

    chore: swap deprecated vim lsp function

--- nvim/lua/daniel/plugins/lualine.lua
+++ nvim/lua/daniel/plugins/lualine.lua
@@ -1,7 +1,7 @@
 local components = {
     lsp = {
         function()
-            local buf_clients = vim.lsp.get_active_clients { bufnr = 0 }
+            local buf_clients = vim.lsp.get_clients { bufnr = 0 }
             if #buf_clients == 0 then
                 return "LSP Inactive"
             end
