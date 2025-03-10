b9e4f391e72dfa0e055478e56b89f42aee36df0c
Daniel Pereira <mail@danielpereira.xyz>
2025-03-10 23:59:29 +0000

    fix: disable bootloader update

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,7 +18,6 @@
 	# Bootloader.
 	boot.loader.systemd-boot.enable = true;
 	boot.loader.efi.canTouchEfiVariables = true;
-	boot.loader.systemd-boot.update = false;
 
 	networking.hostName = "nixos"; # Define your hostname.
 	# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
