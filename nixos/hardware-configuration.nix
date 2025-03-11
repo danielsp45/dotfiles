1237cf7e9a54834251607b5edd81ad56b9234117
Daniel Pereira <mail@danielpereira.xyz>
2025-03-11 15:21:54 +0000

    fix[nixos]: borg sudo config package

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -32,15 +32,16 @@
 
 	# Bakcup configuration
 	system.activationScripts.borgInit = let
-		borg = pkgs.borgbackup;
+	  borg = pkgs.borgbackup;
+	  sudo = pkgs.sudo;
 	in ''
-  export PATH=${borg}/bin:$PATH
-  if [ ! -d /mnt/data/backup/repository.d ]; then
-	echo "Borg repository not found, initializing..."
-	sudo -u daniel ${borg}/bin/borg init --encryption=repokey /mnt/data/backup
-  else
-	echo "Borg repository already initialized."
-  fi
+	  export PATH=${borg}/bin:${sudo}/bin:$PATH
+	  if [ ! -d /mnt/data/backup/repository.d ]; then
+		echo "Borg repository not found, initializing..."
+		sudo -u daniel ${borg}/bin/borg init --encryption=repokey /mnt/data/backup
+	  else
+		echo "Borg repository already initialized."
+	  fi
 	'';
 
 	swapDevices = [ ];
