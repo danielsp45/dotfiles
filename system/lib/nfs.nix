4a5dd65141a417ab8de155dcdc9bec02ef7dbffd
Daniel Pereira <mail@danielpereira.xyz>
2026-05-06 10:27:08 +0100

    fix[nfs]: mount point

--- system/lib/nfs.nix
+++ system/lib/nfs.nix
@@ -1,6 +1,6 @@
 {
   serverAddress = "100.81.92.40";
-  exportPath = "/storage";
+  exportPath = "/mnt/storage";
   mountPath = "/mnt/nas";
   # Tailscale CGNAT range — restrict further (e.g. to your LAN) if needed
   allowedNetwork = "100.64.0.0/10";
